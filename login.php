0899a6db94476825fa82acfe66e395df67e1ce9a
Damian10012 <58724198+Damian10012@users.noreply.github.com>
2020-12-15 14:42:38 +0100

    Adding Login system

---
+++ login.php
@@ -0,0 +1,8 @@
+<?php
+
+$username = $_POST['login'];
+$password = $_POST['password'];
+$conn = mysqli_connect("sql4.webzdarma.cz:3306", "helpdeskkval2958","7IG5L*23)v0H3TE3U5^1","helpdeskkval2958");
+$sql = 'SELECT ';
+mysqli_query($conn, $sql);
+?>
