ee0b4dfd19029d0763d04f01d7cfea4824339b4d
Damian10012 <kubista.tomas@ssakhk.cz>
2020-12-16 11:47:57 +0100

    table2.php once again

--- table2.php
+++ table2.php
@@ -1,30 +1,25 @@
-
-
-
-
-
-<div class="part1"></div>
-<div class="tabulka">
-<table border ="1" >
-
-    <td> ID </td>
-    <td> Zaměstnanec </td>
-    <td> Datum </td>
-   
-    <td> Problém </td>
-    <td> Řešení</td>
-    <td> Důležitost </td>
-    <td> Stav</td>
-   
-</tr> 
 <?php
 $conn = mysqli_connect("sql4.webzdarma.cz:3306", "suppsystemkv6540","ptRl6qp7%5*tk.@7,3Wb","suppsystemkv6540");
-$sql = "SELECT ID,zamestnanec,datum,problem,reseni,dulezitost,stav FROM  wo";
-$result =mysqli_query($conn,$sql);
+$sql = "SELECT ID,zamestnanec,datum,problem,reseni,dulezitost,stav FROM  wo";?>
+<div class="tabulka">
+    <div class="part1"></div>'
+    <div class="tabulka">
+    <table border ="1" >
+        <tr>
+            <td> ID </td>
+            <td> Zaměstnanec </td>
+            <td> Datum </td>   
+            <td> Problém </td>
+            <td> Řešení</td>
+            <td> Důležitost </td>
+            <td> Stav</td>
+        </tr> 
+<?php
 $pocitadlo=0;
 if($result->num_rows>0) {
     while (($row = $result->fetch_assoc())&&$pocitadlo<=9 )
     {
+        
         echo "</br>";
         echo "<tr>";
         echo "<td>".$row['ID'] ."</td>";
