4937ff711a2435372530098be7a7a1a0918a3f1f
Damian10012 <kubista.tomas@ssakhk.cz>
2020-12-16 09:43:00 +0100

    Adding WO script

---
+++ WOscript.js
@@ -0,0 +1,61 @@
+        var POST = false;
+        function Ticket()
+        {
+            
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange=function(){
+            if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("WorkSpace").innerHTML = this.responseText;
+                }
+            };
+            xhttp.open("GET", "Ticket_2.html", true);
+            xhttp.send();           
+        }
+        /*function Dulezitost(Imp){
+            if(Imp==""){
+                alert("Vyberte dulezitost");
+            }
+            else
+                var Importance = Imp;            
+            }*/
+        function FunctSend(){
+                
+                
+               
+                var Name = document.getElementById("Name").value;
+                var Problem = document.getElementById("Problem").value;
+                var Imp = document.getElementById("Imp").value;
+                if(Imp=="" || Name ==""||Problem=="")
+                {
+                    alert("Zadejte data");
+                    return;
+                }
+                
+                var xhttp = new XMLHttpRequest();
+                
+               
+                
+                xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("WorkSpace").innerHTML = this.responseText;
+                }
+                };
+                xhttp.open("POST","Table.php", true);
+                xhttp.send(Name+Problem+Imp); 
+                
+                
+            }       
+             
+        function Table()
+        {
+            
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("WorkSpace").innerHTML = this.responseText;
+                }
+            };
+            xhttp.open("GET", "Table.php", true);
+            xhttp.send();
+        
+        }
